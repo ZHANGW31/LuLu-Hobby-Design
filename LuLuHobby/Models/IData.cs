eac22aaca1f2a02ede90d6395bd675c69d2e078a
Wei Zhang <wei.zhang4@outlook.com>
2020-04-04 22:16:51 -0700

    Added Fake, and TestDouble Classes, and IData interface.

---
+++ LuLuHobby/Models/IData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LuLuHobby.Models
+{
+    public interface IData
+    {
+        //Data model interface implementation here
+    }
+}
---
+++ LuLuHobby/Models/TestDouble.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LuLuHobby.Models
+{
+    public class TestDouble : IData
+    {
+        //Test double implementation here.
+    }
+}
